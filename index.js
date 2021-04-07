5d29591d2389cea8da9d7d66306ca2741cb9b1f9
Garret <gwiats@gmail.com>
2021-04-07 19:51:40 -0400

    first two passing

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(content, test) {
+      if (Array.isArray(content) === true) {
+        for (let i = 0; i < content.length; i++) {
+          test(content[i])
+        }
+      } else {
+          for (const key in content) {
+            test(content[key])
+          }
+      }
+      return content
     },
 
-    map: function() {
-
+    map: function(data, action) {
+      let newData = eachFor(data, action)
+      return newData
     },
 
     reduce: function() {
@@ -25,3 +35,17 @@ const fi = (function() {
 })()
 
 fi.libraryMethod()
+
+function eachFor(content, test) {
+  const result = []
+  if (Array.isArray(content) === true) {
+    for (let i = 0; i < content.length; i++) {
+      result.push(test(content[i]))
+    }
+  } else {
+    for (const key in content) {
+      result.push(test(content[key]))
+    }
+  }
+  return result
+}
